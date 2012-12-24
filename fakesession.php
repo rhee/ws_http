44c67397718d7ff5a70b46406402e14809aee9e5
Sang-Hoon RHEE <rhee@websync.co.kr>
2012-12-24 17:42:33 +0900

    .

--- fakesession.php
+++ fakesession.php
@@ -1,16 +1,20 @@
 <?php
 if(PHP_SAPI=='cli'){
+
+  define('FAKESESSIONFILE',getenv("phpfakesession")?getenv("phpfakesession"):'session.json');
+
   function fakesession_load() {
-    $_SESSION=json_decode(file_get_contents("session.json"),true);
+    $_SESSION=json_decode(file_get_contents(FAKESESSIONFILE),true);
   }
   function fakesession_save() {
-    if(isset($_SESSION)&&count($_SESSION)>0)file_put_contents("session.json",json_encode($_SESSION));
+    if(isset($_SESSION)&&count($_SESSION)>0)file_put_contents(FAKESESSIONFILE,json_encode($_SESSION));
   }
   function onexit(){fakesession_save();}
   register_shutdown_function('onexit');
 
-  if(file_exists("session.json")){
+  if(file_exists(FAKESESSIONFILE)){
     @session_start();
     fakesession_load();
   }
+
 }
